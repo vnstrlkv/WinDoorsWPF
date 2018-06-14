2829c0f2727a83b12902d63aad6fefd74280bd94
vnstrlkv <vnstrlkv@gmail.com>
2018-06-14 15:29:33 +0700

    1

--- WinDoorsWPF/Model/Person.cs
+++ WinDoorsWPF/Model/Person.cs
@@ -8,5 +8,9 @@ namespace WinDoorsWPF.Model
 {
     class Person
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public List<Window> Windows { get; set; }
     }
 }
--- WinDoorsWPF/Model/Window.cs
+++ WinDoorsWPF/Model/Window.cs
@@ -34,5 +34,9 @@ namespace WinDoorsWPF.Model
         {
             return Height * Width;
         }
+
+
+        
+
     }
 }
