6c963c26a7e95d93e671d210daf2cda25c1bbf90
vnstrlkv <vnstrlkv@gmail.com>
2018-06-14 14:04:57 +0700

    1

--- WinDoorsWPF/Model/Window.cs
+++ WinDoorsWPF/Model/Window.cs
@@ -8,6 +8,31 @@ namespace WinDoorsWPF.Model
 {
    public class Window
     {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public string Material { get; set; }
+        
 
+        public bool Deaf { get; set; }
+        public bool OpenWindow { get; set; }
+        public bool Pipe { get; set; }
+        public bool PaintPipe { get; set; }
+        public bool Flash { get; set; }
+
+        public bool FullOpenWindow { get; set; }
+
+        public bool Cutting { get; set; }
+
+
+
+        public double Perimeter()
+        {
+            return (Height + Width) * 2;
+        }
+
+        public double Square()
+        {
+            return Height * Width;
+        }
     }
 }
