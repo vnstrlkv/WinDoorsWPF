74c4addf43a5d74a12718750379e07cb799413fb
vnstrlkv <vnstrlkv@gmail.com>
2018-06-14 13:32:20 +0700

    Add

---
+++ WinDoorsWPF/Model/Material.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinDoorsWPF.Model
+{
+    class Material
+    {
+        private string name;
+        private string metr;
+        private double price;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Metr
+        {
+            get { return metr; }
+            set { metr = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        
+    }
+}
---
+++ WinDoorsWPF/Model/PriceList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinDoorsWPF.Model
+{
+    class PriceList
+    {
+        List<Material> Materials { get; set; }
+        List<Material> Service { get; set; }
+
+
+
+        
+    }
+}
